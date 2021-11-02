fe8e98e97366751bfd0e0a137eb593e1fa67cc7f
ClementG60 <cle_guillaume@hotmail.fr>
2021-11-02 16:54:33 +0100

    ajout code

---
+++ ex5.js
@@ -0,0 +1,26 @@
+let day = Math.floor(Math.random () * 7) + 1
+switch (day) {
+    case 1 :
+        alert("Lundi")
+        break
+    case 2 :
+        alert("Mardi")
+        break
+    case 3 :
+        alert("Mercredi")
+        break
+    case 4 :
+        alert("Jeudi")
+        break
+    case 5 :
+        alert("Vendredi")
+        break
+    case 6 :
+        alert("Samedi")
+        break
+    case 7 :
+        alert("Dimanche")
+        break
+    default :
+        alert("Aucun chiffre ne correspond")
+}
